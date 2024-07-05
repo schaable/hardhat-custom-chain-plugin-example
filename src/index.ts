eeb7967202edee38927d58acbbb74b0198a7a51c
Luis Schaab <schaable@gmail.com>
2024-07-05 12:02:30 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import "@nomicfoundation/hardhat-verify";
+import { extendConfig } from "hardhat/config";
+import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
+
+extendConfig(
+  (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
+    config.etherscan.customChains.push({
+      network: "mychain",
+      chainId: 123456,
+      urls: {
+        apiURL: "https://mychainscan.io/api",
+        browserURL: "https://mychainscan.io",
+      },
+    });
+  }
+);
